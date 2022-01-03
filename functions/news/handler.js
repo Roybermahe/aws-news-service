de0864978525e8a044358ff2d4ae59600b1a60de
Royber <roybermanjarrez@gmail.com>
2022-01-03 17:03:25 -0500

    add function getObeBySource  and getOneByMentions

--- functions/news/handler.js
+++ functions/news/handler.js
@@ -1,9 +1,28 @@
 var petition = require('../../share/libs/libExternalPetition');
 var messages = require('../../share/libs/libProcessMesages');
 
-module.exports.hello= async (event, context) => {
+module.exports.getNews= async (event, context) => {
   try {
-    return await petition.get('https://newsapi.org/v2/everything?q=tesla&from=2021-12-02&sortBy=publishedAt&apiKey=14f70045a6184c80ac2aa986d2f53a17');
+    return await petition.get('https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=14f70045a6184c80ac2aa986d2f53a17');
+  } catch (error) {
+    return messages.error(error.message);
+  }
+}
+
+module.exports.getOneBySource = async (event, context) => {
+  try {
+    const param = event.pathParameters;
+    return await petition.get(`https://newsapi.org/v2/top-headlines?sources=${param.source}&apiKey=14f70045a6184c80ac2aa986d2f53a17`);
+  } catch (error) {
+    return messages.error(error.message);
+  }
+}
+
+module.exports.getOneByMentions = async (event, context) => {
+  try {
+    if(!event.queryStringParameters.q) throw 'Not found parameter';
+    const param = event.queryStringParameters.q;
+    return await petition.get(`https://newsapi.org/v2/everything?q=${param}&sortBy=publishedAt&apiKey=14f70045a6184c80ac2aa986d2f53a17`);
   } catch (error) {
     return messages.error(error.message);
   }
